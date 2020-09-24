b5d80f1e53814a6a288e057549a3c2bba7283847
dkippes <kippes.diego@gmail.com>
2020-09-24 17:46:49 -0300

    updated express-generator

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-let app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.listen(3000, () =>{ 
-    console.log('Servidor Andando!');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', function(req, res) {
-    res.send('Bienvenido al home de BierClub!');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
