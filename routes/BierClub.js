cd4969cc923174560b30945a21b07afecaf36979
dkippes <kippes.diego@gmail.com>
2020-09-28 18:51:21 -0300

    home, y ruta de promociones

--- routes/BierClub.js
+++ routes/BierClub.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('BierClub');
+});
+
+/* GET Promociones page. */
+router.get('/promociones', function(req, res, next) {
+  res.render('promociones');
 });
 
 module.exports = router;
