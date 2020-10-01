2fd28bda58b7760767461a7e1c2d632ed77c5203
dkippes <kippes.diego@gmail.com>
2020-10-01 03:39:40 -0300

    rutas envios, login, productos, quiense somos, registro agregadas

--- routes/BierClub.js
+++ routes/BierClub.js
@@ -6,9 +6,36 @@ router.get('/', function(req, res, next) {
   res.render('BierClub');
 });
 
+/* GET Productos page. */
+router.get('/productos', function(req, res, next) {
+  res.render('productos');
+});
+
 /* GET Promociones page. */
 router.get('/promociones', function(req, res, next) {
   res.render('promociones');
 });
+
+/* GET Envios page. */
+router.get('/envios', function(req, res, next) {
+  res.render('envios');
+});
+
+/* GET Quienes Somos page. */
+router.get('/quienes_somos', function(req, res, next) {
+  res.render('quienes_somos');
+});
+
+/* GET Registro page. */
+router.get('/registro', function(req, res, next) {
+  res.render('registro');
+});
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+
 
 module.exports = router;
