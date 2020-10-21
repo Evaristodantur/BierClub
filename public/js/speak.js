b4dd7200f0fa533fcc59713b1e70bf9f61604fae
dkippes <kippes.diego@gmail.com>
2020-10-21 05:39:19 -0300

    actualizado

---
+++ public/js/speak.js
@@ -0,0 +1,6 @@
+let titulo = document.title;
+let texto = `Hola, soy tu amigo el hombre araña y entraste a ${titulo}`;
+        const hablar = (texto) => speechSynthesis.
+        speak(new SpeechSynthesisUtterance(texto));
+
+        hablar(texto);
