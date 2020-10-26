5b00f1ad67b8e0afb916f4b54acb52f34d9493b7
Evaristo Dantur <evaristo@dantur.com>
2020-10-26 16:59:37 -0300

    onClick en el hamburguesa funcionando

---
+++ public/js/nav.js
@@ -0,0 +1,9 @@
+/* ONCLICK HAMBURGUESA ABRIR */
+function hamburguesa(){
+    document.getElementById("hamburguesa-onclick").style = "left:10px;";
+  }
+
+/* ONCLICK HAMBURGUESA CERRAR */
+  function hamburguesaCerrar(){
+    document.getElementById("hamburguesa-onclick").style = "left:-350px;";
+  }
