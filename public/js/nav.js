14c652aaae36484ffb4fc99c4a08e568cba45f25
Evaristo Dantur <evaristo@dantur.com>
2020-10-27 02:32:18 -0300

    nav mejorado, icono cambiado, cambios en el menu hamburguesa

--- public/js/nav.js
+++ public/js/nav.js
@@ -25,19 +25,21 @@ function hamburguesa(){
 NAV BAR FUNCTION SCROLL TO TOP
 ========================================================================== */
 
-  /*     window.onscroll = function() {scrollFunction()};
+/*       window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("navbar").style.margin = "-72px 0px 0px 0px";
-        document.getElementById("logonav").style.display = "inline-block";
-        document.getElementById("navcarrito").style.margin = "32px 0px 0px 0px";
-        document.getElementById("navlogin").style.margin = "32px 0px 0px 0px";
-      } else {
-        document.getElementById("navbar").style.margin = "0px 0px 0px 0px";
-        document.getElementById("logonav").style.display = "none";
-        document.getElementById("navcarrito").style.margin = "-35px 0px 0px 0px";
-        document.getElementById("navlogin").style.margin = "-35px 0px 0px 0px";
+      if (window.innerWidth > 1200) {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          document.getElementById("navbar").style.margin = "-72px 0px 0px 0px";
+          document.getElementById("logonav").style.display = "inline-block";
+          document.getElementById("navcarrito").style.margin = "32px 0px 0px 0px";
+          document.getElementById("navlogin").style.margin = "32px 0px 0px 0px";
+        } else {
+          document.getElementById("navbar").style.margin = "0px 0px 0px 0px";
+          document.getElementById("logonav").style.display = "none";
+          document.getElementById("navcarrito").style.margin = "-35px 0px 0px 0px";
+          document.getElementById("navlogin").style.margin = "-35px 0px 0px 0px";
+        }
       }
     } */
 
