025f42610a1736d6c030f88b04bfd2d3339a0a76
Evaristo Dantur <evaristo@dantur.com>
2020-10-27 20:33:13 -0300

    script funcionando por ahora, tengo que ver el nav1

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,10 +2,10 @@
 HAMBURGUESA
 ========================================================================== */
 
-/* ONCLICK HAMBURGUESA ABRIR 
+/* ONCLICK HAMBURGUESA ABRIR */
 function hamburguesa(){
     document.getElementById("hamburguesa-onclick").style = "left:10px;";
-  } */
+  }
 /* ONCLICK HAMBURGUESA CERRAR */
   function hamburguesaCerrar(){
     document.getElementById("hamburguesa-onclick").style = "left:-350px;";
