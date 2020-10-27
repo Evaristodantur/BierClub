a18ae8e0e822bd0593cdb6f2d167401bb890e50f
Evaristo Dantur <evaristo@dantur.com>
2020-10-27 20:31:52 -0300

    script comentado

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,21 +2,20 @@
 HAMBURGUESA
 ========================================================================== */
 
-/* ONCLICK HAMBURGUESA ABRIR */
+/* ONCLICK HAMBURGUESA ABRIR 
 function hamburguesa(){
     document.getElementById("hamburguesa-onclick").style = "left:10px;";
-  }
-
+  } */
 /* ONCLICK HAMBURGUESA CERRAR */
   function hamburguesaCerrar(){
     document.getElementById("hamburguesa-onclick").style = "left:-350px;";
   }
 
 /* ONCLICK HAMBURGUESA DROPDOWN */
-
   function hamburguesaDropdown() {
     document.getElementById("hamburguesa-dropdown").classList.toggle("showProductos");
   }
+
   function transform() {
     document.getElementById("transform").classList.toggle("transform");
   }
