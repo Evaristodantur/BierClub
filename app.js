12d1dfac59bb03893e0c7f5714aa794f99c56133
dkippes <kippes.diego@gmail.com>
2020-11-03 20:20:04 -0300

    route products agregada en app y creada en la carpeta route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/BierClub');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/products.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+//Controladores
+let productsController = require('../controllers/productsController');
+
+
+
+module.exports = router;
