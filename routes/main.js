94714df394fe61b34d376c93c040485d6adcb791
dkippes <kippes.diego@gmail.com>
2020-11-03 20:32:50 -0300

    login y register agregados al controlador de users

--- routes/main.js
+++ routes/main.js
@@ -2,22 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-/* GET Registro page. */
-router.get('/register', function(req, res, next) {
-  res.render('users/register');
-});
-
-/* GET Login page. */
-router.get('/login', function(req, res, next) {
-  res.render('users/login');
-});
-
 /* GET About us page. */
-router.get('/about-us', function(req, res, next) {
+router.get('/about-us', (req, res, next) => {
   res.render('aboutUs');
 });
 
