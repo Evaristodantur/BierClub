7461b29795ac02c8bc554d4667e1b859fc2e96fc
Evaristo Dantur <evaristo@dantur.com>
2020-11-03 20:36:55 -0300

    merge solucionado

--- routes/main.js
+++ routes/main.js
@@ -5,13 +5,7 @@ var router = express.Router();
 let mainController = require('../controllers/mainController');
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/', mainController.index);
-=======
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
->>>>>>> 94714df394fe61b34d376c93c040485d6adcb791
 
 /* GET About us page. */
 router.get('/about-us', (req, res, next) => {
