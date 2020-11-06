dab66a3e6238d4ffedbd217ecdf99e516f9af6b3
dkippes <kippes.diego@gmail.com>
2020-11-06 02:37:18 -0300

    method override instalado y agregado a dependencias

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
