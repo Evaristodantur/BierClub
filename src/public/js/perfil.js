7a83a04d41c19abe8d377602ba6fd73903334af4
Evaristo Dantur <evaristo@dantur.com>
2020-11-06 05:33:01 -0300

    onclick de modificar funcionando

---
+++ src/public/js/perfil.js
@@ -0,0 +1,20 @@
+/* ==========================================================================
+HAMBURGUESA
+========================================================================== */
+
+/* ONCLICK HAMBURGUESA ABRIR */
+function hamburguesa(){
+    document.getElementById("hamburguesa-onclick").style = "top:60%;";
+  }
+
+/* ONCLICK HAMBURGUESA CERRAR */
+  function hamburguesaCerrar(){
+    document.getElementById("hamburguesa-onclick").style = "top:47%;";
+  }
+
+/* ONCLICK HAMBURGUESA DROPDOWN */
+
+  function hola() {
+    document.getElementById("modificar").classList.toggle("showModificar");
+  }
+
