0471b9bce1ab87eaf56fbb07a3f3d91624c9a5cc
dkippes <kippes.diego@gmail.com>
2020-11-17 02:19:37 -0300

    carpeta de middlewares, y multer trasladada a la misma

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,22 +1,12 @@
 let express = require('express');
 let router = express.Router();
-let multer = require('multer');
-let path = require('path');
 
 //Controller
 let productsController = require('../controllers/productsController');
 
-//Almacenamiento de las imagenes
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './src/public/images/productos')
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'producto' + '-' + Date.now() + path.extname(file.originalname));
-    }
-  });
-   
-  var upload = multer({ storage: storage });
+//Middleware
+let upload = require('../middlewares/multer');  //Multer
+const { check, validationResult, body } = require('express-validator'); //Express-validator
 
 
 
@@ -47,7 +37,7 @@ router.get('/productAdmin', productsController.productAdmin);
 /* GET     /products/productAdd - Pagina Visual de create               page. */
 router.get('/productAdd', productsController.createProduct);
 
-/* GET     /products/productAdd - Crea/almacena el producto             page. */
+/* POST     /products/productAdd - Crea/almacena el producto             page. */
 router.post('/productAdd', upload.any(), productsController.storeProduct);
 
 /**********************************PRODUCT EDIT***************************************/
@@ -55,7 +45,7 @@ router.post('/productAdd', upload.any(), productsController.storeProduct);
 /* GET     /products/productEdit/:id - Pagina Visual de productEdit     page. */
 router.get('/productEdit/:id', productsController.editProduct);
 
-/* GET     /products/productEdit/:id - Modifica el producto             page. */
+/* PUT     /products/productEdit/:id - Modifica el producto             page. */
 router.put('/productEdit/:id', upload.any(), productsController.updateProduct);
 
 /**********************************BORRAR*********************************************/
