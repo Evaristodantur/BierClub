9cec5096fb1d18a9e69509ed2b576830798e4319
dkippes <kippes.diego@gmail.com>
2020-11-17 20:13:20 -0300

    notEmpty agregado a productsMiddleware

--- src/middlewares/productosMiddleware.js
+++ src/middlewares/productosMiddleware.js
@@ -2,10 +2,10 @@ let path = require('path');
 const { check, validationResult, body } = require('express-validator'); //Express-validator
 
 let productMiddleware =  [
-        check('nombre').isLength({min:3, max:100}).withMessage('debe tener entre 3 y 100 caracteres'),
-        check('precio').isInt({min:0}).withMessage('debe ser un numero positivo y no estar vacio'),
-        check('descuento').isInt({min:0, max:100}).withMessage('debe ser un numero del 0 al 100'),
-        check('stock').isInt({min:0}).withMessage('debe ser un numero positivo y no estar vacio'),
+        check('nombre').notEmpty().withMessage('no puede estar vacio').isLength({min:3, max:100}).withMessage('debe tener entre 3 y 100 caracteres'),
+        check('precio').notEmpty().withMessage('no puede estar vacio').isInt({min:0}).withMessage('debe ser un numero positivo'),
+        check('descuento').notEmpty().withMessage('no puede estar vacio').isInt({min:0, max:100}).withMessage('debe ser un numero del 0 al 100'),
+        check('stock').notEmpty().withMessage('no puede estar vacio').isInt({min:0}).withMessage('debe ser un numero positivo'),
         check('descripcion').isLength({max:280}).withMessage('solo acepta hasta 280 caracteres')
 ]
 
