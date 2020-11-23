32824f9b09155b4a838068ea0a05acf7e72fe51d
Evaristo Dantur <evaristo@dantur.com>
2020-11-23 07:02:06 -0300

    pagina de error completada

--- src/middlewares/authAdminMiddleware.js
+++ src/middlewares/authAdminMiddleware.js
@@ -2,7 +2,7 @@ let authAdminMiddleware = (req, res, next) => {
     if (req.session.usuarioLogueado.admin == true) {
         next();
     } else {
-        res.send("Necesitas ser admin para ver esta pagina");
+        res.render("sinPermisos");
     }
 }
 
