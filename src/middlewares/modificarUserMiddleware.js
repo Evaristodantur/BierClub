ed00e52c67b63175d185f4f8d5e16a9d53442f36
Evaristo Dantur <evaristo@dantur.com>
2020-11-25 04:36:30 -0300

    'direccion email ya registrada' en perfil problema resuelto

--- src/middlewares/modificarUserMiddleware.js
+++ src/middlewares/modificarUserMiddleware.js
@@ -16,33 +16,64 @@ let modificarUserMiddleware = [
     check("confirmarContrasenia").notEmpty().withMessage("Este campo no puede estar vacío")
         .isLength({min:8}).withMessage("Este campo debe tener al menos 8 caracteres"),
         checkSchema({
-            confirmarContrasenia: {
-                    custom: {
-                            options: (value, { req }) => {
-                                if(req.body.contrasenia == req.body.confirmarContrasenia){
-                                    return true;
-                                  }
-                                  return false;                                
-                            }
-                    },
-                    errorMessage : 'Las contraseñas no coinciden'
-            }
-    }),
-    checkSchema({
-        antiguaContrasenia: {
+            email: {
                 custom: {
                         options: (value, { req }) => {
                             let idUrl = req.params.id;
-                            let usuarioOriginal = usuariosJson.find( usuario => usuario.id == idUrl);
-                            if(!bcrypt.compareSync(req.body.antiguaContrasenia,usuarioOriginal.contrasenia)){
-                                return false;
+                            let usuarioBuscado = usuariosJson.find( usuario => usuario.id == idUrl );
+
+                            for(let i = 0 ; i < usuariosJson.length ; i++){
+                                if(req.body.email == usuariosJson[i].email && req.body.email != usuarioBuscado.email){
+                                    return false;
+                                }
                             }
-                                return true;                                
+                            return true;                                
                         }
+                    },
+                errorMessage : 'Esta dirección email ya esta registrada'
+            }
+        }),
+        checkSchema({
+            contrasenia: {
+                custom: {
+                    options: (value, { req }) => {
+                        if(req.body.contrasenia === req.body.email){
+                            return false;
+                        }
+                        return true;                                
+                    }
                 },
-                errorMessage : 'La antigua contraseña no coincide'
-        }
-})
+            errorMessage : 'La contraseña no puede ser igual al email'
+            }
+        }),
+        checkSchema({
+            confirmarContrasenia: {
+                custom: {
+                        options: (value, { req }) => {
+                            if(req.body.contrasenia == req.body.confirmarContrasenia){
+                                return true;
+                                }
+                                return false;                                
+                        }
+                    },
+                errorMessage : 'Las contraseñas no coinciden'
+            }
+        }),
+        checkSchema({
+            antiguaContrasenia: {
+                custom: {
+                    options: (value, { req }) => {
+                        let idUrl = req.params.id;
+                        let usuarioOriginal = usuariosJson.find( usuario => usuario.id == idUrl);
+                        if(!bcrypt.compareSync(req.body.antiguaContrasenia,usuarioOriginal.contrasenia)){
+                            return false;
+                        }
+                            return true;                                
+                        }
+                    },
+                 errorMessage : 'La antigua contraseña no coincide'
+            }
+        })
 ]
 
 module.exports = modificarUserMiddleware
