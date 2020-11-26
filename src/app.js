5dd573956d180f2ae179c4e0418fe8fcbb7a6aee
Evaristo Dantur <evaristo@dantur.com>
2020-11-26 02:45:50 -0300

    problema resuelto

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var methodOverride = require('method-override');
 var session = require('express-session');
 
-
 //Middlewares generales
 var cookieAuthMiddleware = require("./middlewares/cookieAuthMiddleware") 
 
