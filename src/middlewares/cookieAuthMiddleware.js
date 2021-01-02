309f531b1dfb1f3e11d1a5037457aa2f07996f57
dkippes <kippes.diego@gmail.com>
2021-01-02 19:48:03 -0300

    cookieAuthMiddleware ready

--- src/middlewares/cookieAuthMiddleware.js
+++ src/middlewares/cookieAuthMiddleware.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const db = require("../database/models");
 
 let usuariosJson = fs.readFileSync(path.resolve(__dirname, '../database/usuarios.json'), 'utf-8');
 
@@ -10,9 +11,16 @@ let cookieAuthMiddleware = (req, res, next) => {
     if (req.cookies.recordame != undefined && 
         req.session.usuarioLogueado == undefined) {
 
-            let usuarioALoguearse = usuariosJson.find(usuario => usuario.email == req.cookies.recordame);
+            db.Users.findOne({
+                where: {
+                    email: req.cookies.recordame
+                }
+            }).then(user => {
+                req.session.usuarioLogueado = user
+            })
+            /* let usuarioALoguearse = usuariosJson.find(usuario => usuario.email == req.cookies.recordame);
 
-            req.session.usuarioLogueado = usuarioALoguearse;
+            req.session.usuarioLogueado = usuarioALoguearse; */
     }
     next();
 }
