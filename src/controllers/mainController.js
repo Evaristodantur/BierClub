f88f71747f8e7b902951da8c8be04349a66a39b5
dkippes <kippes.diego@gmail.com>
2021-01-06 00:33:30 -0300

    mainController identacion updated

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,97 +6,104 @@ require("dotenv").config();
 let db = require("../database/models");
 let sequelize = db.sequelize;
 
-//Agregado database JSON
-let productsJson = fs.readFileSync(path.resolve(__dirname, '../database/products.json'), 'utf-8');
-let dbDirectory = path.resolve(__dirname, '../database/products.json');
-
-productsJson == "" ?
-    fs.writeFileSync(dbDirectory, JSON.stringify(productsJson = [])) :
-    productsJson = JSON.parse(fs.readFileSync(dbDirectory, 'utf-8'));
-
     
 let mainController = {
     
+
     //  Home
     index : (req, res, next) => {
 
+        //Muestra todos los productos en el home
         db.Products.findAll({
             include: [{association: "images"}]
-        }).then(function(products){
+        }).then(products => {
             res.render('index', { productos : products });
-        })
-
-        /* function arrayDesordenado(array) {
-            let elementoActual = array.length, almacenamientoTemporal, random;
-          
-            // Mientras queden elementos sin desordenar
-            while (0 !== elementoActual) {
-          
-              // Agarra un elemento y lo hace random
-              random = Math.floor(Math.random() * elementoActual);
-              elementoActual -= 1;
-          
-              // lo cambia por el elemento actual
-              almacenamientoTemporal = array[elementoActual];
-              array[elementoActual] = array[random];
-              array[random] = almacenamientoTemporal;
-            }
-            return array;
-          } */
-
-        //res.render('index', {productos : arrayDesordenado(productsJson)});
+        }).catch(error => {
+            console.log(error);
+        });
     },
+
+
+
+
 
     //  /about-us
     aboutUs : (req, res, next) => {
         res.render('aboutUs', {title : "hola"});
     },
 
+
+
+
+
     //  /promociones
     promociones : (req, res, next) => {
         res.render('promociones');
     },
+
+
+
+
 
     //  /suscripcion
     suscripcion : (req, res, next) => {
         res.render('suscripcion');
     },
 
-    // ERROR
+
+
+
+
+    // Pagina de error
     error : (req, res, next) => {
         res.render('error');
     },
-    //ENVIOS
+
+
+
+
     
+    // /envios
     envios : (req,res, next) => { 
         res.render('envios');
     },
 
 
 
-    //Contacto
+
+
+    // /contacto
     contactoRender : (req, res, next) => {
         res.render("contacto")
-      },
+    },
 
+
+
+
+    // Post de Contacto
     contactoSend : (req, res, next) => {
-    // Enviar errores express-validator
-    let errores = validationResult(req);
-    errores.reqNombre = req.body.nombre;
-    errores.reqEmail = req.body.email;
-    errores.reqSubject = req.body.subject;
-    errores.reqMessage = req.body.message;
-    if (!errores.isEmpty()){
-        return res.render("contacto", {errors : errores})
-    }
 
-    let nodemailerAssets = require("../assets/nodemailerAssets");
+        // Enviar errores express-validator
+        let errores = validationResult(req);
 
-    nodemailerAssets(req).transporter.sendMail(nodemailerAssets(req).mailOptions, function(err, data){
-        if(!err){
-        res.render("contacto", { success : "Tu mensaje fue enviado con exito!" })
+        errores.reqNombre = req.body.nombre;
+        errores.reqEmail = req.body.email;
+        errores.reqSubject = req.body.subject;
+        errores.reqMessage = req.body.message;
+
+        //Verifica si hay errores
+        if (!errores.isEmpty()){
+            return res.render("contacto", {errors : errores})
         }
-    })
+
+        //Manda el mensaje de contacto a bierclub
+        let nodemailerAssets = require("../assets/nodemailerAssets");
+
+        nodemailerAssets(req).transporter.sendMail(nodemailerAssets(req).mailOptions, function(err, data){
+            if(!err){
+                res.render("contacto", { success : "Tu mensaje fue enviado con exito!" })
+            }
+        });
     }
 } 
 
