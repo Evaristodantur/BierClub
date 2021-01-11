d462c85d0476d1ece955d2ab984a0bb1b94e1fcf
dkippes <kippes.diego@gmail.com>
2021-01-11 05:18:47 -0300

    prueba de agregado a carrito superada

--- src/controllers/cartsController.js
+++ src/controllers/cartsController.js
@@ -1,20 +1,24 @@
+let fs = require('fs');
+let path = require('path');
 let db = require("../database/models");
 const {validationResult} = require("express-validator");
+const e = require("express");
 
 let mainController = {
     index: (req, res, next) => {
-         db.Carts.findAll()
-            .then(carts => {
-                res.send(carts)
+         db.Carts.findAll({
+             include: [{association: 'products'}]
+         })
+            .then(carritos => {
+                res.send(carritos)
             })
     },
     addProduct: (req, res, next) => {
 
-        let userLogged = req.session.usuarioLogueado
-
-        console.log(req.session.usuarioLogueado);
+        let userLogged = req.session.usuarioLogueado        
 
         if(userLogged) {
+            
             db.Carts.findOne({
                 where: {
                     user_id: userLogged.id,
@@ -22,20 +26,56 @@ let mainController = {
                 },
                 include: [{association: 'products'}]
             }).then(cart => {
-
+                
                 //Si el usuario logeado no tiene un carrito
                 if(cart == null) {
-                    if(userLogged) {
-                        db.Carts.create({
+                    
+                        Promise.resolve(db.Carts.create({
                             user_id: userLogged.id,
-                            status: 0
-                        })
-                    }
+                            status: 0,
+                            cart_product: req.params.id
+                        }))
+                        
                 } else {
-                    res.send(cart)
+                    
+                    //Agrega el producto en un carrito que ya tiene
+                    db.Cart_Product.create({
+                        product_id: req.params.id,
+                        cart_id: cart.id
+                    })
+
+                    res.redirect('/carts')
                 }
-        
+
             })
+
+
+
+            //Agrega el producto despues de crear el carrito en la DB
+            /* db.Carts.findOne({
+                where: {
+                    user_id: userLogged.id,
+                    status: 0
+                },
+                include: [{association: 'products'}]
+            }).then(cart => {
+                
+                //Si el usuario logeado no tiene un carrito
+                
+                    //Agrega el producto en un carrito que ya tiene
+                    db.Cart_Product.create({
+                        product_id: req.params.id,
+                        cart_id: cart.id
+                    })
+
+                    
+                
+
+            }) */
+
+            res.redirect('/carts')
+
+
         } else {
             res.redirect('/users/login')
         }
---
+++ src/database/models/Cart_Product.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Cart_Product";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_id: {
+            type: dataTypes.INTEGER
+        },
+        cart_id: {
+            type: dataTypes.INTEGER
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt: {
+            type: dataTypes.DATE
+        }
+    };
+    let config = {
+        tableName: "cart_product",
+        timestamps: false
+    };
+
+    const Product = sequelize.define(alias, cols, config);
+
+    
+
+    return Product;
+}
