77cea290fd6cd468aebde682eef821dafdc80806
dkippes <kippes.diego@gmail.com>
2021-01-14 03:17:47 -0300

    game updated

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,4 +1,6 @@
 let game = document.addEventListener('DOMContentLoaded', () => {
+
+    
     
     const barney = document.querySelector('.barney');
     const grid = document.querySelector('.grid');
@@ -6,27 +8,39 @@ let game = document.addEventListener('DOMContentLoaded', () => {
     let gravity = 0.9;
     let isGameOver = false;
     let score = 0;
-    let alert = document.getElementById('alert')
+    let viewWidth = window.innerWidth;
 
+    if(viewWidth <= 1200) {
+        window.location = "http://localhost:3000/";
+        window.alert('Este juego funciona bien solo para dispositivos con mas de 1200px');
+        
+    } else {
+        window.alert('Para jugar con la tecla "enter" o "espacio", estas listo?');
+    }
+
+
+    
+
+    //Control de salto
     function controlJump(e) {
         //32 es Espacio-Enter
         if(e.keyCode === 32) {
             //Si esta saltando, no va a volver a saltar
             if(!isJumping) {
                 isJumping = true;
-                jump()
+                jump();
             }
         }
     }
 
-    document.addEventListener('keyup', controlJump)
+    document.addEventListener('keypress', controlJump);
 
-
+    
     //BARNEY
-    let position = 0
+    let position = 0;
     //Salto
     function jump() {
-        let count = 0
+        let count = 0;
         //Despues de 20mil salta
         let barneyHaciaArriba = setInterval(function() {
 
@@ -34,18 +48,18 @@ let game = document.addEventListener('DOMContentLoaded', () => {
             //Cuando barney llega a 15px
             if(count === 25) {
                 //Se para
-                clearInterval(barneyHaciaArriba)
+                clearInterval(barneyHaciaArriba);
                 //Empieza a bajar hasta que llega a la posicion 0
                 let barneyHaciaAbajo = setInterval(function() {
                     if(count === 0) {
-                        clearInterval(barneyHaciaAbajo)
+                        clearInterval(barneyHaciaAbajo);
                         //Setea el salto para saltar, porque ya esta en el suelo
                         isJumping = false;
                     }
                     position -= 1
-                    count--
-                    position *= gravity
-                    barney.style.bottom = position + 'px'
+                    count--;
+                    position *= gravity;
+                    barney.style.bottom = position + 'px';
                 }, 20)
                 
             }
@@ -54,9 +68,9 @@ let game = document.addEventListener('DOMContentLoaded', () => {
             //Si no se complio los 150p, barney sigue subiendo
             console.log('up');
             position += 50; //Salta 30px
-            count++ //Cuenta cuantas veces salto
+            count++; //Cuenta cuantas veces salto
             position *= gravity; //Le agrega la gravedad
-            barney.style.bottom = position + 'px'
+            barney.style.bottom = position + 'px';
         }, 20)
     }
 
@@ -64,37 +78,59 @@ let game = document.addEventListener('DOMContentLoaded', () => {
 
     //Creacion de los MOES
     function generateMoes() {
-        let randomTime = Math.random() * 4000
+        let randomTime = Math.random() * 4000;
+        let moePosition;
+
+              
+        //Para diferentes resoluciones de pantalla hasta 1200px
+        //1600px para arriba
+        if(viewWidth > 1600) {
+            moePosition = 1600;
+        }
+
+        //1600 - 1500px
+        if(viewWidth < 1600 && viewWidth > 1500) {
+            moePosition = 1200;
+        }
+
+        //1500 - 1300px
+        if(viewWidth < 1500 && viewWidth > 1300) {
+            moePosition = 1000;
+        }
+
+        //1300 - 1200px
+        if(viewWidth < 1300 && viewWidth > 1200) {
+            moePosition = 950;
+        }
+        
 
         //Creo un div, con la clase del obstaculo
-        let moePosition = 1600
-        const moe = document.createElement('div')
+        const moe = document.createElement('div');
 
         //Si el juego no termino, agrego otro obstaculo
         if(!isGameOver) {
-            moe.classList.add('moeStyle')
+            moe.classList.add('moeStyle');
             
         }
-        grid.appendChild(moe)
-        moe.style.left = moePosition + 'px'
+        grid.appendChild(moe);
+        moe.style.left = moePosition + 'px';
 
         //Cada cierto tiempo sale otro moe
         let moeHaciaLaIzquierda = setInterval(function(){
 
             //Se fija si no se chocan
-            if(moePosition > 10 && moePosition < 250 && position < 250) {
-                clearInterval(moeHaciaLaIzquierda)
+            if(moePosition > 6 && moePosition < 180 && position < 180) {
+                clearInterval(moeHaciaLaIzquierda);
                 
-                //alert.innerHTML = 'Game Over, Your score: ' + score 
-                isGameOver = true
+                isGameOver = true;
 
                 if(isGameOver) {
                     if(window.confirm(`Tu score fue: ${score}, ¿Desea volver a jugar?`)) {
-                        window.location = "http://localhost:3000/game"
+                        window.location = "http://localhost:3000/game";
                     } else {
                         //Remueve todos los hijos
                         while(grid.firstChild) {
-                            grid.removeChild(grid.lastChild)
+                            grid.removeChild(grid.lastChild);
                         }
                     }
                 }
@@ -102,23 +138,24 @@ let game = document.addEventListener('DOMContentLoaded', () => {
                 
             }
 
-            moePosition -= 15
-            moe.style.left = moePosition + 'px'
+            moePosition -= 15;
+            moe.style.left = moePosition + 'px';
 
             //Suma puntos
-            if(moePosition == 10) {
+            if(moePosition < 10 && moePosition > -10 || moePosition == 0) {
                 score++;
             }
             
         },20)
-
         //Si el juego no termino sigue andando
         if(!isGameOver) {
-            setTimeout(generateMoes, randomTime)
+            setTimeout(generateMoes, randomTime);
         }
         
     }
 
+    
+
     //Genera mas moes
-    generateMoes()
+    generateMoes();
 })
