49d4b2064c8e8d4750af6f919a14cc32352dabe0
dkippes <kippes.diego@gmail.com>
2021-01-19 01:59:50 -0300

    slider updated

--- src/public/js/productsSlider.js
+++ src/public/js/productsSlider.js
@@ -6,60 +6,52 @@ window.onload = function() {
     const products = document.querySelectorAll('#productArticle')
     let count = 0;
 
-    if(window.innerWidth >= 1200) {
-        for(let i=0; i < products.length; i++) {
-            if(i > 4) {
-                products[i].style.display = 'none'
+
+    display_Initial_ProductsView(window.innerWidth >= 1400, 4)
+    display_Initial_ProductsView(window.innerWidth < 1400, 3)
+    display_Initial_ProductsView(window.innerWidth <= 1024, 2)
+    display_Initial_ProductsView(window.innerWidth <= 768, 1)
+    display_Initial_ProductsView(window.innerWidth < 525, 0)
+
+    function display_Initial_ProductsView(width, howManyProducts) {
+        if(width) {
+            for(let i=0; i < products.length; i++) {
+                if(i > howManyProducts) {
+                    products[i].style.display = 'none'
+                }
             }
         }
     }
 
-    if(window.innerWidth < 1200) {
-        for(let i=0; i < products.length; i++) {
-            if(i > 3) {
-                products[i].style.display = 'none'
+    display_Products()
+
+    function display_Products(width, howManyProducts) {
+        if(width) {
+            if(typeof products[count+howManyProducts] != "undefined") {
+                products[count+howManyProducts].style.display = "flex"
+                products[count].style.display = "none";
+                count++;
+            } else {
+                count = 0;
+                for(let i=0; i < products.length; i++) {
+                    if(i > (howManyProducts-1)) {
+                        products[i].style.display = 'none'
+                    } else {
+                        products[i].style.display = 'flex'
+                    }
+                }
             }
         }
     }
 
     next.addEventListener('click', ()=> {
         console.log(products[count+5]);
-        if(window.innerWidth >= 1200) {
-            if(typeof products[count+5] != "undefined") {
-                console.log(products[count+5]);
-                products[count+5].style.display = "flex"
-                products[count].style.display = "none";
-                count++;
-            } else {
-                count = 0;
-                for(let i=0; i < products.length; i++) {
-                    if(i > 4) {
-                        products[i].style.display = 'none'
-                    } else {
-                        products[i].style.display = 'flex'
-                    }
-                }
-            }
-        }
 
-        if(window.innerWidth < 1200) {
-            if(typeof products[count+4] != "undefined") {
-                console.log(products[count+4]);
-                products[count+4].style.display = "flex"
-                products[count].style.display = "none";
-                count++;
-            } else {
-                count = 0;
-                for(let i=0; i < products.length; i++) {
-                    if(i > 3) {
-                        products[i].style.display = 'none'
-                    } else {
-                        products[i].style.display = 'flex'
-                    }
-                }
-            }
-        }
-            
+        display_Products(window.innerWidth >= 1400, 5)
+        display_Products(window.innerWidth < 1400 && window.innerWidth >= 1024, 4)
+        display_Products(window.innerWidth < 1024 && window.innerWidth >= 768, 3)
+        display_Products(window.innerWidth < 768 && window.innerWidth >= 525, 2)
+        display_Products(window.innerWidth < 525, 1)
     })
 
     prev.addEventListener('click', ()=> {
