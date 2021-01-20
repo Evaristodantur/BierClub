ecbeee460db6b4f78d57c9f1bf5d0fdf5d74c5c7
dkippes <kippes.diego@gmail.com>
2021-01-20 00:04:22 -0300

    productSlider de Novedades en index

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -30,8 +30,74 @@ window.addEventListener('load', () => {
             });
         }
     }
-    
+
+    function productosNovedadesSlider() {
+        const next = document.getElementById('product-next')
+        const prev = document.getElementById('product-prev')
+        const products = document.querySelector('.productsSlider')
+        const allProducts = document.querySelectorAll('.productNovedades');
+
+        //Clono los nodos para poder hacer filtrado de novedades
+        let cloneProducts = products.cloneNode(true);
+        //Lo doy vuelta
+        var arrayNodes = Array.from(cloneProducts.children).reverse()
+
+        //Los cambio por los productos en general
+        for(let i=0; i < arrayNodes.length; i++) {
+            allProducts[i].innerHTML = arrayNodes[i].innerHTML
+        }
+
+        //Los muestro como vista inicial
+        display_Initial_ProductsView(window.innerWidth >= 1400, 4)
+        display_Initial_ProductsView(window.innerWidth < 1400 && window.innerWidth > 1024, 3)
+        display_Initial_ProductsView(window.innerWidth <= 1024 && window.innerWidth > 768, 2)
+        display_Initial_ProductsView(window.innerWidth <= 768 && window.innerWidth > 525, 1)
+        display_Initial_ProductsView(window.innerWidth <= 525, 0)
+        
+
+        prev.addEventListener('click', () => {
+            products.insertBefore(products.lastElementChild, products.firstElementChild)
+            products.firstElementChild.style.display = "flex"
+            
+            nextOrPrevProduct__InWidth_(5, window.innerWidth >= 1400, "none")
+            nextOrPrevProduct__InWidth_(4, window.innerWidth < 1400 && window.innerWidth > 1024, "none")
+            nextOrPrevProduct__InWidth_(3, window.innerWidth <= 1024 && window.innerWidth > 768, "none")
+            nextOrPrevProduct__InWidth_(2, window.innerWidth <= 768 && window.innerWidth > 525, "none")
+            nextOrPrevProduct__InWidth_(1, window.innerWidth <= 525, "none")
+            
+        })
+
+        next.addEventListener('click', ()=> {
+
+            products.appendChild(products.firstElementChild)
+            products.lastElementChild.style.display = "none"
+
+            nextOrPrevProduct__InWidth_(4, window.innerWidth >= 1400, "flex")
+            nextOrPrevProduct__InWidth_(3, window.innerWidth < 1400 && window.innerWidth >= 1024, "flex")
+            nextOrPrevProduct__InWidth_(2, window.innerWidth <= 1024 && window.innerWidth > 768, "flex")
+            nextOrPrevProduct__InWidth_(1, window.innerWidth <= 768 && window.innerWidth > 525, "flex")
+            nextOrPrevProduct__InWidth_(0, window.innerWidth <= 525, "flex")
+
+        })
+
+        function nextOrPrevProduct__InWidth_(children, width, childrenStatus) {
+            if(width) {
+                products.children[children].style.display = childrenStatus
+            }
+        }
+
+        function display_Initial_ProductsView(width, howManyProducts) {
+            if(width) {
+                for(let i=0; i < allProducts.length; i++) {
+                    if(i > howManyProducts) {
+                        allProducts[i].style.display = 'none'
+                    }
+                }
+            }
+        }
+    }
 
     //Ejecucion de Funciones
+    productosNovedadesSlider();
     sendNewsletterNotification();
 });
