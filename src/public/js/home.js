5cb913a67a2d54b6d76ebb1f1b62b457b35674f3
dkippes <kippes.diego@gmail.com>
2021-01-20 02:29:14 -0300

    productSlider especificado y js de home documentado

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -4,33 +4,33 @@ window.addEventListener('DOMContentLoaded', () => {
    MODAL DE NEWSLETTER NOTIFICATION
    ========================================================================== */
     function sendNewsletterNotification() {
-        let modalNewsletter = document.getElementById('modal-newsletter')
-        let buttonNewsletter = document.querySelector('#modal-newsletter button');
-        let divNewsletter = document.querySelector('#modal-newsletter div.background-newsletter-modal div')
+        let $modalNewsletter = document.getElementById('modal-newsletter')
+        let $buttonNewsletter = document.querySelector('#modal-newsletter button');
+        let $divNewsletter = document.querySelector('#modal-newsletter div.background-newsletter-modal div')
 
-        if(modalNewsletter != null) {
-            modalNewsletter.style.display = 'inline-block';
+        if($modalNewsletter != null) {
+            $modalNewsletter.style.display = 'inline-block';
 
             //Modal para todas las resoluciones
-            if(window.innerWidth <= 1200) { divNewsletter.style.left = '40%'; }
-            if(window.innerWidth <= 900) { divNewsletter.style.left = '37%'; }
-            if(window.innerWidth <= 750) { divNewsletter.style.left = '32%'; }
-            if(window.innerWidth <= 600) { divNewsletter.style.left = '25%'; }
-            if(window.innerWidth <= 500) { divNewsletter.style.left = '20%'; }
-            if(window.innerWidth <= 400) { divNewsletter.style.left = '12%'; }
-            if(window.innerWidth <= 350) { divNewsletter.style.left = '6%'; }
-            if(window.innerWidth <= 325) { divNewsletter.style.width = '263px'; divNewsletter.style.left = '9%'; }
-            if(window.innerWidth <= 315) { divNewsletter.style.left = '6%'; }
-            if(window.innerWidth <= 300) { divNewsletter.style.left = '1%'; }
-            if(window.innerWidth <= 290) { divNewsletter.style.left = '0%'; }
+            if(window.innerWidth <= 1200) { $divNewsletter.style.left = '40%'; }
+            if(window.innerWidth <= 900) { $divNewsletter.style.left = '37%'; }
+            if(window.innerWidth <= 750) { $divNewsletter.style.left = '32%'; }
+            if(window.innerWidth <= 600) { $divNewsletter.style.left = '25%'; }
+            if(window.innerWidth <= 500) { $divNewsletter.style.left = '20%'; }
+            if(window.innerWidth <= 400) { $divNewsletter.style.left = '12%'; }
+            if(window.innerWidth <= 350) { $divNewsletter.style.left = '6%'; }
+            if(window.innerWidth <= 325) { $divNewsletter.style.width = '263px'; $divNewsletter.style.left = '9%'; }
+            if(window.innerWidth <= 315) { $divNewsletter.style.left = '6%'; }
+            if(window.innerWidth <= 300) { $divNewsletter.style.left = '1%'; }
+            if(window.innerWidth <= 290) { $divNewsletter.style.left = '0%'; }
             
             
-            buttonNewsletter.addEventListener('click', () => {
-                modalNewsletter.style.display = 'none';
+            $buttonNewsletter.addEventListener('click', () => {
+                $modalNewsletter.style.display = 'none';
             });
             
             document.querySelector('#modal-newsletter div.background-newsletter-modal').addEventListener('click', () =>{
-                modalNewsletter.style.display = 'none';
+                $modalNewsletter.style.display = 'none';
             });
         }
     }
@@ -41,19 +41,19 @@ window.addEventListener('DOMContentLoaded', () => {
    SLIDER DE PRODUCTOS CON NOVEDAD
    ========================================================================== */
     function productosNovedadesSlider() {
-        const next = document.getElementById('product-next');
-        const prev = document.getElementById('product-prev');
-        const products = document.querySelector('.productsSlider');
-        const allProducts = document.querySelectorAll('.productNovedades');
+        const $nextProductNovedad = document.getElementById('product-next');
+        const $prevProductNovedad = document.getElementById('product-prev');
+        const $productsNovedad = document.querySelector('.productsSlider');
+        const $allProductsNovedad = document.querySelectorAll('.productNovedades');
 
         //Clono los nodos para poder hacer filtrado de novedades
-        let cloneProducts = products.cloneNode(true);
+        let cloneProducts = $productsNovedad.cloneNode(true);
         //Lo doy vuelta
         var arrayNodes = Array.from(cloneProducts.children).reverse();
 
         //Los cambio por los productos en general
         for(let i=0; i < arrayNodes.length; i++) {
-            allProducts[i].innerHTML = arrayNodes[i].innerHTML;
+            $allProductsNovedad[i].innerHTML = arrayNodes[i].innerHTML;
         }
 
         //Los muestro como vista inicial
@@ -65,9 +65,9 @@ window.addEventListener('DOMContentLoaded', () => {
         
 
         //Boton de siguiente producto
-        prev.addEventListener('click', () => {
-            products.insertBefore(products.lastElementChild, products.firstElementChild);
-            products.firstElementChild.style.display = "flex";
+        $prevProductNovedad.addEventListener('click', () => {
+            $productsNovedad.insertBefore($productsNovedad.lastElementChild, $productsNovedad.firstElementChild);
+            $productsNovedad.firstElementChild.style.display = "flex";
             
             nextOrPrevProduct__InWidth_(5, window.innerWidth >= 1400, "none");
             nextOrPrevProduct__InWidth_(4, window.innerWidth < 1400 && window.innerWidth > 1024, "none");
@@ -79,9 +79,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         //Boton de anterior producto
-        next.addEventListener('click', ()=> {
-            products.appendChild(products.firstElementChild)
-            products.lastElementChild.style.display = "none"
+        $nextProductNovedad.addEventListener('click', ()=> {
+            $productsNovedad.appendChild($productsNovedad.firstElementChild)
+            $productsNovedad.lastElementChild.style.display = "none"
 
             nextOrPrevProduct__InWidth_(4, window.innerWidth >= 1400, "flex")
             nextOrPrevProduct__InWidth_(3, window.innerWidth < 1400 && window.innerWidth >= 1024, "flex")
@@ -93,16 +93,16 @@ window.addEventListener('DOMContentLoaded', () => {
         //Funcion para pasar al siguiente u anterior producto
         function nextOrPrevProduct__InWidth_(children, width, childrenStatus) {
             if(width) {
-                products.children[children].style.display = childrenStatus
+                $productsNovedad.children[children].style.display = childrenStatus
             }
         }
 
         //Function de muestra inicial de los productos al primer loading
         function display_Initial_ProductsView(width, howManyProducts) {
             if(width) {
-                for(let i=0; i < allProducts.length; i++) {
+                for(let i=0; i < $allProductsNovedad.length; i++) {
                     if(i > howManyProducts) {
-                        allProducts[i].style.display = 'none'
+                        $allProductsNovedad[i].style.display = 'none'
                     }
                 }
             }
@@ -115,19 +115,19 @@ window.addEventListener('DOMContentLoaded', () => {
    SLIDER DE PRODUCTOS EN STOCK
    ========================================================================== */
     function productosStockSlider() {
-        const nextProductFilter = document.querySelector('.btnSig-Filter');
-        const prevProductFilter = document.querySelector('.btnAnt-Filter');
-        const productsFilter = document.querySelector('.productFilter');
+        const $nextProductFilter = document.querySelector('.btnSig-Filter');
+        const $prevProductFilter = document.querySelector('.btnAnt-Filter');
+        const $productsFilter = document.querySelector('.productFilter');
         
         //Borro todo lo que no tiene stock
-        for(let i=0; i < productsFilter.children.length; i++) {
-            if(productsFilter.children[i].querySelector(".sinStock") != null) {
-                productsFilter.removeChild(productsFilter.children[i]);
+        for(let i=0; i < $productsFilter.children.length; i++) {
+            if($productsFilter.children[i].querySelector(".sinStock") != null) {
+                $productsFilter.removeChild($productsFilter.children[i]);
             }
         }
         //Checkea el ultimo elemento si no tiene stock - ya que el for no lo hace
-        if(productsFilter.children[productsFilter.children.length-1].querySelector(".sinStock") != null) {
-            productsFilter.removeChild(productsFilter.children[productsFilter.children.length-1]);
+        if($productsFilter.children[$productsFilter.children.length-1].querySelector(".sinStock") != null) {
+            $productsFilter.removeChild($productsFilter.children[$productsFilter.children.length-1]);
         }
             
         //Los muestro como vista inicial
@@ -139,9 +139,9 @@ window.addEventListener('DOMContentLoaded', () => {
         
         
         //Boton de siguiente producto
-        nextProductFilter.addEventListener('click', ()=> {
-            productsFilter.appendChild(productsFilter.firstElementChild);
-            productsFilter.lastElementChild.style.display = "none";
+        $nextProductFilter.addEventListener('click', ()=> {
+            $productsFilter.appendChild($productsFilter.firstElementChild);
+            $productsFilter.lastElementChild.style.display = "none";
 
             nextOrPrevProductStock__InWidth_(4, window.innerWidth >= 1400, "flex");
             nextOrPrevProductStock__InWidth_(3, window.innerWidth < 1400 && window.innerWidth >= 1024, "flex");
@@ -151,9 +151,9 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         //Boton de anterior producto
-        prevProductFilter.addEventListener('click', ()=> {
-            productsFilter.insertBefore(productsFilter.lastElementChild, productsFilter.firstElementChild);
-            productsFilter.firstElementChild.style.display = "flex";
+        $prevProductFilter.addEventListener('click', ()=> {
+            $productsFilter.insertBefore($productsFilter.lastElementChild, $productsFilter.firstElementChild);
+            $productsFilter.firstElementChild.style.display = "flex";
 
             nextOrPrevProductStock__InWidth_(5, window.innerWidth >= 1400, "none");
             nextOrPrevProductStock__InWidth_(4, window.innerWidth < 1400 && window.innerWidth > 1024, "none");
@@ -165,9 +165,9 @@ window.addEventListener('DOMContentLoaded', () => {
         //Function de muestra inicial de los productos al primer loading
         function display_Initial_ProductsStockView(width, howManyProducts) {
             if(width) {
-                for(let i=0; i < productsFilter.children.length; i++) {
+                for(let i=0; i < $productsFilter.children.length; i++) {
                     if(i > howManyProducts) {
-                        productsFilter.children[i].style.display = 'none';
+                        $productsFilter.children[i].style.display = 'none';
                     }
                 }
             }
@@ -176,7 +176,7 @@ window.addEventListener('DOMContentLoaded', () => {
         //Funcion para pasar al siguiente u anterior producto
         function nextOrPrevProductStock__InWidth_(children, width, childrenStatus) {
             if(width) {
-                productsFilter.children[children].style.display = childrenStatus;
+                $productsFilter.children[children].style.display = childrenStatus;
             }
         }
     }
