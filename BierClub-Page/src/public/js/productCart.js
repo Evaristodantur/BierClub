07fd1bea07eda302b7775f1eb9a8478db0dc8b6c
dkippes <kippes.diego@gmail.com>
2021-02-22 01:07:30 -0300

    borrado de todos los productos en productCart arreglado

--- BierClub-Page/src/public/js/productCart.js
+++ BierClub-Page/src/public/js/productCart.js
@@ -1,134 +1,92 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-    /* window.onresize = function() {
+  /* window.onresize = function() {
         location.reload()
     } */
-    
-    let tagsH3Productos = document.querySelectorAll('.tagsEspecificosDeProductos h3');
-    if(window.innerWidth < 525) {
-        for (let i = 0; i < tagsH3Productos.length; i++) {
-            tagsH3Productos[i].style.display = 'none';
-        }    
-        
-        let productPrice = document.querySelectorAll('.producto-precio');
-        let productQuantity = document.querySelectorAll('.producto-cantidad');
-        let productSubTotal = document.querySelectorAll('.producto-precio-total');
-        for(let i=0; i < productPrice.length; i++) {
-            productPrice[i].remove();
-            productQuantity[i].remove();
-            productSubTotal[i].remove();
-        }
 
+  let tagsH3Productos = document.querySelectorAll(
+    '.tagsEspecificosDeProductos h3'
+  );
+  if (window.innerWidth < 525) {
+    for (let i = 0; i < tagsH3Productos.length; i++) {
+      tagsH3Productos[i].style.display = 'none';
     }
 
+    let productPrice = document.querySelectorAll('.producto-precio');
+    let productQuantity = document.querySelectorAll('.producto-cantidad');
+    let productSubTotal = document.querySelectorAll('.producto-precio-total');
+    for (let i = 0; i < productPrice.length; i++) {
+      productPrice[i].remove();
+      productQuantity[i].remove();
+      productSubTotal[i].remove();
+    }
+  }
 
-    //Borrado de todos los productos
-    let btnBorrarProducto = document.querySelector('.btnBorrarProducto');
+  //Borrado de todos los productos
+  let modalBorrarTodosLosProductos = document.querySelector(
+    '.modal-deleteAllProducts'
+  );
+
+  let btnBorrarTodosLosProductos = document.getElementById(
+    'borrarTodosLosProducts'
+  );
+
+  let noBorrarTodosLosProductos = document.getElementById(
+    'closeModalDeleteAllProducts'
+  );
+
+  btnBorrarTodosLosProductos.addEventListener('click', () => {
+      modalBorrarTodosLosProductos.style.display = 'block'
+  })
+
+  noBorrarTodosLosProductos.addEventListener('click', () => {
+      modalBorrarTodosLosProductos.style.display = 'none'
+  });
+
+  window.addEventListener('click', (e) => {
+      if (e.target == modalBorrarTodosLosProductos) {
+          modalBorrarTodosLosProductos.style.display = 'none';
+      }
+  })
+
+  /* MODAL - DELETE PRODUCT */
+  let xProductoABorrar = document.querySelectorAll('.deleteProduct');
+    console.log(xProductoABorrar);
+  for (let i = 0; i < xProductoABorrar.length; i++) {
     
-    btnBorrarProducto.addEventListener('click', (e) => {
-        let modalBorrarTodosLosProductos = document.querySelector('.showModalAllProductosBeforeDelete');
-        modalBorrarTodosLosProductos.style.display = 'inline-block';
-        
-        modalBorrarTodosLosProductos.querySelector(".noBtnDeleteAll").addEventListener("click", () => {
-            modalBorrarTodosLosProductos.style.display = 'none';
-        });
-    });
 
-    let xProductoABorrar = document.querySelectorAll('.deleteProduct');
-    for(let i=0; i < xProductoABorrar.length; i++) {
-        xProductoABorrar[i].addEventListener('click', () => {
-            
-            let modalProductABorrar = xProductoABorrar[i].nextElementSibling;
-            modalProductABorrar.style.display = "inline-block";
-            let divBackground = document.createElement("div");
-            document.body.insertAdjacentElement('afterbegin', divBackground);
-            console.log(divBackground);
-            
-            modalProductABorrar.querySelector('.noBtnDeleteProduct').addEventListener('click', () => {
-                modalProductABorrar.style.display = 'none';
-            });
-        })
+    //Mostrar Subtotal
+    let stocks = document.querySelectorAll('#stockProducto');
+    let total = document.querySelector('.totalAPagar');
+    let productos = document.querySelectorAll('.producto-carrito');
 
-
-
-        
-
-        //Mostrar Subtotal
-        let stocks = document.querySelectorAll("#stockProducto");
-        let total = document.querySelector('.totalAPagar');
-        let productos = document.querySelectorAll(".producto-carrito");
-
-        function muestraDeSubtotal() {
-          let totalInicial = 0;
-          for (let i = 0; i < productos.length; i++) {
-            if (productos[i].style.display != 'none') {
-              let subtotal = productos[i].querySelector(
-                '.producto-precio-total'
-              );
-              if (subtotal) {
-                totalInicial += parseInt(subtotal.innerHTML.substr(1));
-              }
-            }
+    function muestraDeSubtotal() {
+      let totalInicial = 0;
+      for (let i = 0; i < productos.length; i++) {
+        if (productos[i].style.display != 'none') {
+          let subtotal = productos[i].querySelector('.producto-precio-total');
+          if (subtotal) {
+            totalInicial += parseInt(subtotal.innerHTML.substr(1));
           }
-          return (total.innerHTML = '$' + totalInicial);
         }
-        
-        muestraDeSubtotal()
-        
-        
-        
-
-        stocks.forEach(stock => {
-            stock.addEventListener('change', (e) => {
-            console.log(stock);
-              let price = stock.parentElement.querySelector('.producto-precio');
-              let subtotal = stock.parentElement.querySelector(
-                '.producto-precio-total'
-              );
-              subtotal.innerHTML =
-                '$' +
-                parseInt(price.innerHTML.substr(1)) * parseInt(stock.value);
-
-                muestraDeSubtotal();
-                
-            });
-
-            
-        })
-        
+      }
+      return (total.innerHTML = '$' + totalInicial);
     }
-    
-    
-    
-    
-    
-    /* if(window.innerWidth < 768 ) {
-        
-        let productos = document.querySelectorAll(".producto-carrito");
-        for(let i=0; i < productos.length; i++) {
 
-            crearDivConClase_En_ConTexto_('.producto-precio', productos[i], 'Precio:');
+    muestraDeSubtotal();
 
-            crearDivConClase_En_ConTexto_('.producto-cantidad', productos[i], 'Cantidad:');
+    stocks.forEach((stock) => {
+      stock.addEventListener('change', (e) => {
+        console.log(stock);
+        let price = stock.parentElement.querySelector('.producto-precio');
+        let subtotal = stock.parentElement.querySelector(
+          '.producto-precio-total'
+        );
+        subtotal.innerHTML =
+          '$' + parseInt(price.innerHTML.substr(1)) * parseInt(stock.value);
 
-            crearDivConClase_En_ConTexto_('.producto-precio-total', productos[i], 'Subtotal:');
-            
-        }
+        muestraDeSubtotal();
+      });
+    });
+  }
 
-        function crearDivConClase_En_ConTexto_(claseABuscar, elemento, textoDeh4) {
-            let tagABuscar = elemento.querySelector(claseABuscar);
-            let div = document.createElement('div');
-            let h4 = document.createElement('h4');
-            let text = document.createTextNode(textoDeh4);
-            h4.appendChild(text);
-            div.appendChild(h4);
-            div.classList.add('divMobileProductCart');
-            tagABuscar.parentElement.replaceChild(div, tagABuscar);
-            div.insertAdjacentElement('beforeend', tagABuscar);
-        }
-        
-
-        
-        
-    } */
 });
